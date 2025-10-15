61ef7a9223778432b20f80b27accfe687d02cee3
gelkoh <ole_kirchner@gmx.de>
2025-10-15 21:21:26 +0200

    Change nvim color scheme

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,7 +1,18 @@
-return {
-    "EdenEast/nightfox.nvim",
-    config = function()
-        vim.cmd("colorscheme terafox")
-    end
-}
+-- return {
+--     "EdenEast/nightfox.nvim",
+--     config = function()
+--         vim.cmd("colorscheme terafox")
+--     end
+-- }
 
+return {
+    "metalelf0/black-metal-theme-neovim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+        require("black-metal").setup({
+            theme = "bathory",
+        })
+        require("black-metal").load()
+    end,
+}
